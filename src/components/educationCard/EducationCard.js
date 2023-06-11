ee696f4ff3842a5a0c636df4067baecaf56c7190
Liminal Pepe <theliminalguy@gmail.com>
2023-06-10 21:15:24 -0300

    update educational layout and info

--- src/components/educationCard/EducationCard.js
+++ src/components/educationCard/EducationCard.js
@@ -8,11 +8,7 @@ export default function EducationCard({school}) {
 
   const GetDescItems = ({descBullets}) => {
     return descBullets
-      ? descBullets.map((item, i) => (
-          <p key={i} className="subTitle">
-            {item}
-          </p>
-        ))
+      ? descBullets.map((item, i) => <p key={i}>{item}</p>)
       : null;
   };
   const {isDark} = useContext(StyleContext);
@@ -21,50 +17,43 @@ export default function EducationCard({school}) {
     console.error(`Image of ${school.name} is missing in education section`);
   return (
     <div>
-      <Fade left duration={1000}>
-        <div className="education-card">
-          {school.logo && (
-            <div className="education-card-left">
-              <img
-                crossOrigin={"anonymous"}
-                ref={imgRef}
-                className="education-roundedimg"
-                src={school.logo}
-                alt={school.schoolName}
-              />
-            </div>
-          )}
-          <div className="education-card-right">
-            <h5 className="education-text-school">{school.schoolName}</h5>
+      <div className="education-card">
+        {school.logo && (
+          <div className="education-card-left">
+            <img
+              crossOrigin={"anonymous"}
+              ref={imgRef}
+              className="education-roundedimg"
+              src={school.logo}
+              alt={school.schoolName}
+            />
+          </div>
+        )}
+        <div className="education-card-right">
+          <h5 className="education-text-school">{school.schoolName}</h5>
 
-            <div className="education-text-details">
-              <h5
-                className={
-                  isDark
-                    ? "dark-mode education-text-subHeader"
-                    : "education-text-subHeader"
-                }
-              >
-                {school.subHeader}
-              </h5>
-              <p
-                className={`${
-                  isDark ? "dark-mode" : ""
-                } education-text-duration`}
-              >
-                {school.duration}
-              </p>
-              <p className="education-text-desc">{school.desc}</p>
-              <div className="education-text-bullets">
-                <GetDescItems descBullets={school.descBullets} />
-              </div>
+          <div className="education-text-details">
+            <h5
+              className={
+                isDark
+                  ? "dark-mode education-text-subHeader"
+                  : "education-text-subHeader"
+              }
+            >
+              {school.subHeader}
+            </h5>
+            <p
+              className={`${isDark ? "dark-mode" : ""} education-text-duration`}
+            >
+              {school.duration}
+            </p>
+            <p className="education-text-desc">{school.desc}</p>
+            <div className="education-text-bullets">
+              <GetDescItems descBullets={school.descBullets} />
             </div>
           </div>
         </div>
-      </Fade>
-      <Slide left duration={2000}>
-        <div className="education-card-border"></div>
-      </Slide>
+      </div>
     </div>
   );
 }
