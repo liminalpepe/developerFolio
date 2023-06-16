47ec35e8b0a0da42eea56f3ee791837abe42690b
Liminal Pepe <theliminalguy@gmail.com>
2023-06-16 16:18:42 -0300

    match foot to figma mockups

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -3,17 +3,18 @@ import "./Footer.scss";
 import {Fade} from "react-reveal";
 import emoji from "react-easy-emoji";
 import StyleContext from "../../contexts/StyleContext";
+import {footer} from "../../portfolio";
 
 export default function Footer() {
   const {isDark} = useContext(StyleContext);
   return (
     <Fade bottom duration={1000} distance="5px">
-      <div className="footer-div">
-        <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
-          {emoji("Made with ❤️ by DeveloperFolio Team")}
-        </p>
-        <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
-          Theme by{" "}
+      <div className={isDark ? "dark-mode footer-div" : "footer-div"}>
+        <p>{footer.title}</p>
+        <p className="quotation-text">{footer.subTitle}</p>
+        <p className="quotation-text">{footer.subQuote}</p>
+        <p className="quottation-text-link">
+          Inspired in{" "}
           <a href="https://github.com/saadpasta/developerFolio">
             developerFolio
           </a>
