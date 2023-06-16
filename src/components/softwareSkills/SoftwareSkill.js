28c509ae7d3ad6bea0cfadbab1b45c1abe488a97
Liminal Pepe <theliminalguy@gmail.com>
2023-06-16 16:18:59 -0300

    update skills to new components

--- src/components/softwareSkills/SoftwareSkill.js
+++ src/components/softwareSkills/SoftwareSkill.js
@@ -1,21 +1,23 @@
 import React from "react";
 import "./SoftwareSkill.scss";
-import {skillsSection} from "../../portfolio";
 
-export default function SoftwareSkill() {
+export default function SoftwareSkill({softwareSkills}) {
   return (
     <div>
       <div className="software-skills-main-div">
         <ul className="dev-icons">
-          {skillsSection.softwareSkills.map((skills, i) => {
+          {softwareSkills.map((skills, i) => {
             return (
               <li
                 key={i}
-                className="software-skill-inline"
+                className="software-skill-item"
                 name={skills.skillName}
               >
-                <i className={skills.fontAwesomeClassname}></i>
-                <p>{skills.skillName}</p>
+                <i
+                  className={skills.fontAwesomeClassname}
+                  title={skills?.skillName}
+                ></i>
+                {skills.skillName ? <p>{skills.skillName}</p> : null}
               </li>
             );
           })}
